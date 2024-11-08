d0a7a215ca93b3c4a6187bf5c1f15c35996bbc65
ThomasDix <tjd44264@Uga.edu>
2024-11-08 17:52:34 -0500

    Created Nav and Home page

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navigation.Module.css';
+
+const Navigation = () => {
+    return (
+        <div className="navbar">
+            <button className="nav-item">
+                <img src='https://cdn.icon-icons.com/icons2/3831/PNG/512/mail_icon_234081.png' alt="mail logo" className="mail-logo" />
+                <span>My Profile</span>
+            </button>
+            <button className="nav-item">
+                <img src= 'https://cdn.iconscout.com/icon/free/png-256/free-message-icon-download-in-svg-png-gif-file-formats--text-communication-conversation-chat-pack-sign-symbols-icons-1976874.png' alt="msg logo" className ="msg-logo" />
+                <span>Find A Roomate</span>
+            </button>
+            <button className="nav-item">
+                <img src= 'https://cdn.iconscout.com/icon/free/png-256/free-myspace-logo-icon-download-in-svg-png-gif-file-formats--social-media-pack-logos-icons-721961.png' alt="people logo" className ="ppl-logo" />
+                <span>Current Roomates</span>
+            </button>
+            <button className="nav-item">
+                <img src= 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Simpleicons_Places_home-symbol.svg' alt="house logo" className ="mail-logo" />
+                <span>Available Spaces</span>
+            </button>
+        </div> 
+    );
+};
+
+export default Navigation;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
 // src/app/pages/Home.tsx
 'use client';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+//components
+import Navigation from '../components/Navigation.jsx'; //nav
 
 const Home: React.FC = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Welcome to the UGA Roommate Finder</h1>
+      <Navigation /> {/* Navigation Bar */}
     </div>
   );
 };
