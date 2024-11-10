e4a0ced2db2242934834f322206d7219496153eb
Isaias Pavon <isaiaspavon@Isaiass-MacBook-Pro.local>
2024-11-10 16:29:08 -0500

    Removed temp form

--- src/pages/FindARoommate.tsx
+++ src/pages/FindARoommate.tsx
@@ -6,14 +6,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //components
 import Navigation from '../components/Navigation.jsx'; //nav
 import Home from '../components/UserDisplay';
-import AddItemForm from '../components/AddItemForm'
 
 const FindARoommate: React.FC = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <Navigation /> {/* Navigation Bar */}
       <Home />
-      <AddItemForm />
     </div>
   );
 };
