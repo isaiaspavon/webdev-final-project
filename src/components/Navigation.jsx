19c4df300edf4efd2a6e284662d51543db68529e
ThomasDix <tjd44264@Uga.edu>
2024-11-09 19:54:11 -0500

    Added links between pages on the site

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,29 +2,45 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import Link from "next/link"
+
 import './Navigation.Module.css';
-import HomePage from '../pages/Home';
-import CurrentRoommate from '../pages/CurrentRoommates';
 
 const Navigation = () => {
     return (
         <div className="navbar">
             <button className="nav-item">
-                <img src='https://cdn.icon-icons.com/icons2/3831/PNG/512/mail_icon_234081.png' alt="mail logo" className="mail-logo" />
-                <span>My Profile</span>
+                <Link href="/Home" passHref>
+                    <div className="nav-link">
+                        <img src="https://cdn.icon-icons.com/icons2/3831/PNG/512/mail_icon_234081.png" alt="mail logo" className="mail-logo" />
+                        <span>My Profile</span>
+                    </div>
+                </Link>
             </button>
             <button className="nav-item">
-                <img src= 'https://cdn.iconscout.com/icon/free/png-256/free-message-icon-download-in-svg-png-gif-file-formats--text-communication-conversation-chat-pack-sign-symbols-icons-1976874.png' alt="msg logo" className ="msg-logo" />
-                <span>Find A Roomate</span>
+                <Link href="/FindARoommate" passHref>
+                    <div className="nav-link">
+                        <img src= 'https://cdn.iconscout.com/icon/free/png-256/free-message-icon-download-in-svg-png-gif-file-formats--text-communication-conversation-chat-pack-sign-symbols-icons-1976874.png' alt="msg logo" className ="msg-logo" />
+                        <span>Find A Roomate</span>
+                    </div>
+                </Link>
             </button>
             <button className="nav-item">
-                <img src= 'https://cdn.iconscout.com/icon/free/png-256/free-myspace-logo-icon-download-in-svg-png-gif-file-formats--social-media-pack-logos-icons-721961.png' alt="people logo" className ="ppl-logo" />
-                <span>Current Roomates</span>
+                <Link href="/CurrentRoommates" passHref>
+                    <div className="nav-link">
+                        <img src= 'https://cdn.iconscout.com/icon/free/png-256/free-myspace-logo-icon-download-in-svg-png-gif-file-formats--social-media-pack-logos-icons-721961.png' alt="people logo" className ="ppl-logo" />
+                        <span>Current Roomates</span>
+                    </div>
+                </Link>
             </button>
             <button className="nav-item">
-                <img src= 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Simpleicons_Places_home-symbol.svg' alt="house logo" className ="mail-logo" />
-                <span>Available Spaces</span>
+                <Link href="/AvailableSpaces" passHref>
+                    <div className="nav-link">
+                        <img src= 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Simpleicons_Places_home-symbol.svg' alt="house logo" className ="mail-logo" />
+                        <span>Available Spaces</span>
+                    </div>
+                </Link>
             </button>
         </div> 
     );
