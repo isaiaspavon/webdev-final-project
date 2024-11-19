7e27e4c10dc32cfc36738a87d809f4bc3cdb5f98
ThomasDix <tjd44264@Uga.edu>
2024-11-19 16:56:15 -0500

    added content to AvailableSpaces.tsx

---
+++ src/components/propertyCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './PropertyCard.Module.css';
+interface Cardprops {
+    imgSrc: string;
+    name: string;
+    address: string;
+}
+
+const Card: React.FC<Cardprops> = ({ imgSrc, name, address }) => {
+  return (
+    <div className="card">
+      <img src={imgSrc} alt={name} className="card-img" />
+      <div className="card-content">
+        <h2 className="card-name">{name}</h2>
+        <p className="card-address">{address}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/AvailableSpaces.tsx
+++ src/pages/AvailableSpaces.tsx
@@ -5,11 +5,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 //components
 import Navigation from '../components/Navigation.jsx'; //nav
+import Top from '../components/Spaces.jsx'; //bottom card
 
 const AvailableSpaces: React.FC = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <Navigation /> {/* Navigation Bar */}
+      <Top /> {/* card with apartments*/}
     </div>
   );
 };
