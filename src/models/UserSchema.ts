041525eeb29cbbf0b9c5788624c62ca9c2dcbb06
ThomasDix <tjd44264@Uga.edu>
2024-11-19 15:05:07 -0500

    added more variables to database

--- src/models/UserSchema.ts
+++ src/models/UserSchema.ts
@@ -1,15 +1,32 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface IUser extends Document {
-    username: String;
+    firstname: String;
+    lastname: String;
     email: String;
     password: String;
+    major: String;
+    pets: Boolean;
+    mindpets: Boolean;
+    tidiness: String;
+    degreelvl: String;
+    gender: String;
+    genderpref: String;
 }
 
 const userSchema = new Schema<IUser>({
-    username: {type: String, required: true},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
+    major: {type: String, required: true},
+    pets: {type: Boolean, required: true},
+    mindpets: {type: Boolean, required: true},
+    tidiness: {type: String, required: true},
+    degreelvl: {type: String, required: true},
+    gender: {type: String, required: true},
+    genderpref: {type: String, required: true},
+
 })
 
 export const User = mongoose.models.User?? mongoose.model("User", userSchema);
